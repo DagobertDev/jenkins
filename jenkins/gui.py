d033517550aa15631588b4f47d92c6777eb7c9c1
Vinci7 <fabi.vinci07@gmail.com>
2020-06-04 22:56:50 +0200

    changed directorys of images and edit buttons

--- jenkins/gui.py
+++ jenkins/gui.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter.font import Font
+
 from PIL import ImageTk, Image
 import speech_to_text
 import jenkins
@@ -10,12 +12,7 @@ canvas.pack()
 
 
 root.title("Jenkins - V1")
-root.iconbitmap("C:\\Users\\Vinci\\Jenkins_Icon.ico")
-
-
-def execute_application(task: str):
-    if task.lower() == "jenkins stop":
-        root.destroy
+root.iconbitmap("../images/Jenkins_Icon.ico")
 
 
 def create_label(task: str):
@@ -25,23 +22,26 @@ def create_label(task: str):
     output.place(x=5, y=180)
 
 
-start_button = Button(root, text="Click on me, to tell me something!",
+boldButton = Font(size=10, weight="bold")
+
+
+start_button = Button(root, text="Click on me,\n" "to tell me something!", font=boldButton,
                       borderwidth=2, relief="solid", bg="#6CA6CD", command=lambda:
-[jenkins.main, create_label(task=speech_to_text.get_input()), execute_application(task=speech_to_text.get_input())])
-start_button.config(height=7, width=69)
+[jenkins.main, create_label(task=speech_to_text.get_input())])
+start_button.config(height=7, width=60)
 start_button.place(x=5, y=20)
 
-speaker = ImageTk.PhotoImage(Image.open("C:\\Users\\Vinci\\Lausprecher2.png"))
+speaker = ImageTk.PhotoImage(Image.open("../images/Lautsprecher2.png"))
 speaker_label = Label(root, image=speaker)
 speaker_label.place(x=380, y=50)
 
 
-end_button = Button(root, text="Jenkins schließen!",
+end_button = Button(root, text="Exit!", font=boldButton,
                     borderwidth=2, relief="solid", bg="#6CA6CD", command=root.destroy)
-end_button.config(height=7, width=69)
-end_button.place(x=5, y=380)
+end_button.config(height=7, width=60)
+end_button.place(x=5, y=378)
 
-exitImage = ImageTk.PhotoImage(Image.open("C:\\Users\\Vinci\\exit4.png"))
+exitImage = ImageTk.PhotoImage(Image.open("../images/exit4.png"))
 exitImage_label = Label(root, image=exitImage)
 exitImage_label.place(x=380, y=415)
 
